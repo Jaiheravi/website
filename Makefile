3160c6f1ec17c2ecac80c2d06affbdd9a3bdb313
Jaime Hernandez <contact@jaime.one>
2025-11-27 16:32:03 -0600

    Initial commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+## NOTE ##################################################
+# I don't want to forget this command but I don't know if
+# this should be added to a Haskell-specific file, so I'm
+# just adding a simple Makefile for now.
+
+.PHONY: build
+build:
+	ghciwatch --enable-eval --log-filter "ghciwatch=warn"
---
+++ src/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Data.Function
+
+(|>) :: a -> (a -> b) -> b
+(|>) = (&) -- Maybe I can create this function myself so I don't need to import a dependency
+
+main :: IO ()
+main = html testContent |> putStrLn
+
+{- $>
+do
+  putStrLn "--------------------------------------------------"
+  main
+  putStrLn "--------------------------------------------------"
+<$ -}
+
+testContent :: String
+testContent = "Hello!"
+
+html :: String -> String
+html content = htmlOpen <> content <> htmlEnd
+
+htmlOpen :: String
+htmlOpen = "<html><body>"
+
+htmlEnd :: String
+htmlEnd = "</body></html>"
